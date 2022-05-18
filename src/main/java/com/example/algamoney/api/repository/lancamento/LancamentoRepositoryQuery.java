80b1d644a41c70a1b1ed745fba49aecf1348c714
FernandoAndradeSilva <fernando.andrade.inf@gmail.com>
2022-05-18 14:28:16 -0300

    finalizando filter pesquisa e paginacao

--- src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -2,11 +2,13 @@ package com.example.algamoney.api.repository.lancamento;
 
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface LancamentoRepositoryQuery  {
 
-    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+    Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 
 }
